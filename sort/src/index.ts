90152216c36b3c45ede2fe51d2630b8a0c97e288
Manish Bhatt <manishbhatt594@gmail.com>
2025-01-24 22:17:39 +0530

    Add sort reusable code for numbers & string

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,3 +1,13 @@
-const logName = (name: string) => console.log(name.toLocaleUpperCase());
+import { CharactersCollection } from "./CharactersCollection";
+import { NumbersCollection } from "./NumbersCollection";
+import { Sorter } from "./Sorter";
 
-logName("मनीष / ಮನೀಷ, Okay?");
+const numbers = new NumbersCollection([10, 3, -5, 0, 2, -1]);
+const sorter1 = new Sorter(numbers);
+sorter1.sort();
+console.log(numbers.data);
+
+const chars = new CharactersCollection("Xbaay");
+const sorter2 = new Sorter(chars);
+sorter2.sort();
+console.log(chars.data);
