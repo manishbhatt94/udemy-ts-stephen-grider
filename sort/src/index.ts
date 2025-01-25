cb1abdab47575fea0f8d6a0b4c240ccb665f0c44
Manish Bhatt <manishbhatt594@gmail.com>
2025-01-25 11:58:11 +0530

    Change Sorter to abstract class and make individual collection classes its child to have sort method available on them directly

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,11 +3,9 @@ import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
 const numbers = new NumbersCollection([10, 3, -5, 0, 2, -1]);
-const sorter1 = new Sorter(numbers);
-sorter1.sort();
+numbers.sort();
 console.log(numbers.data);
 
 const chars = new CharactersCollection("Xbaay");
-const sorter2 = new Sorter(chars);
-sorter2.sort();
+chars.sort();
 console.log(chars.data);
