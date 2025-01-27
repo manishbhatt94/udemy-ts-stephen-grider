033188eb8c5dad96aa087ded1a500c437ca1fa2b
Manish Bhatt <manishbhatt594@gmail.com>
2025-01-27 13:37:55 +0530

    Add Linked List

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,6 @@
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
 
 const numbers = new NumbersCollection([10, 3, -5, 0, 2, -1]);
 numbers.sort();
@@ -9,3 +9,11 @@ console.log(numbers.data);
 const chars = new CharactersCollection("Xbaay");
 chars.sort();
 console.log(chars.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(28);
+linkedList.add(13);
+linkedList.sort();
+linkedList.print();
