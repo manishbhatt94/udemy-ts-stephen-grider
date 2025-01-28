b30594f866f396ebdb407e1b134e1c9cee993370
Manish Bhatt <manishbhatt594@gmail.com>
2025-01-28 18:20:15 +0530

    Refactor CSV file reading and use 2 approaches for the job (inheritance & composition)

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,15 @@
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
+import { MatchReader } from "./MatchReader";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { Summary } from "./Summary";
 
-const reader = new CsvFileReader("football.csv");
-reader.read();
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  }
-  if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
+summary.buildAndPrintReport(matchReader.matches);
