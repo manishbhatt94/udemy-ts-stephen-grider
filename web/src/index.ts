799a52edb7b60a94d6152a937b5d771edeaf5f9e
Manish Bhatt <manishbhatt594@gmail.com>
2025-02-01 15:44:29 +0530

    Set up delegation methods set, fetch & save in User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,12 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "Ra's Al Ghul", age: 650 });
-const age = user.get("age");
-console.log(age);
-user.on("change", () => console.log("event change handler"));
-user.trigger("change");
+const user = new User({ id: 1, name: "Shawn Carter", age: 56 });
+
+user.on("change", () => {
+  console.log("user changed", user);
+});
+user.on("save", () => {
+  console.log("user saved", user);
+});
+
+user.save();
