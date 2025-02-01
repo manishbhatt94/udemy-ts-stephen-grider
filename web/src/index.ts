3de684d3f847415fa361412db2e3e68f1f5afd90
Manish Bhatt <manishbhatt594@gmail.com>
2025-02-01 14:45:58 +0530

    prj:web Start to make User class lean and delegate functionality using composition

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,7 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
-
-user.set({ name: "Jason Bourne", age: 55 });
-user.save();
+const user = new User({ name: "Ra's Al Ghul", age: 650 });
+const age = user.get("age");
+console.log(age);
+user.on("change", () => console.log("event change handler"));
+user.trigger("change");
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,36 +1,33 @@
-import axios, { AxiosResponse } from "axios";
+import { Attributes } from "./Attributes";
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = "http://localhost:3000/users";
+
 export class User {
-  constructor(private data: UserProps) {}
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  get on() {
+    return this.events.on;
   }
 
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get("id")}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
+  get trigger() {
+    return this.events.trigger;
   }
 
-  save(): void {
-    const id = this.get("id");
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post(`http://localhost:3000/users`, this.data);
-    }
+  get get() {
+    return this.attributes.get;
   }
 }
