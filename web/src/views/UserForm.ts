0b47b9e47506361e9099b427968460d16fd004e5
Manish Bhatt <manishbhatt594@gmail.com>
2025-02-02 16:57:11 +0530

    Use model inside UserForm view

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,26 +1,39 @@
 import { User } from "../models/User";
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get("name")}</div>
+        <div>User age: ${this.model.get("age")}</div>
         <label for="username">Username</label>
         <input type="text" name="username" id="username">
-        <button type="button">Signup</button>
+        <button class="set-name" type="button">Change name</button>
+        <button class="set-age" type="button">Set Random Age</button>
       </div>
     `;
   }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
+      "click:button.set-age": this.onSetAgeClick,
+      "click:button.set-name": this.onSetNameClick,
     };
   }
 
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
+
   render(): void {
+    this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
@@ -37,7 +50,15 @@ export class UserForm {
     }
   }
 
-  onButtonClick(): void {
-    console.log("button was clicked");
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector(
+      'input[name="username"]'
+    ) as HTMLInputElement;
+    const name = input.value;
+    this.model.set({ name });
+  };
 }
